ac11140e4b50ec5ec9d8a751bde946a7aba8cc50
Nicolas Damasceno <nicolasbackprogrammer@gmail.com>
2026-08-02 17:22:17 -0300

    fix: add validation to UpdatePerfilDto and password DTOs (required fields, min password length)

--- api/Dtos/Fiscal/CreateFiscalDto.cs
+++ api/Dtos/Fiscal/CreateFiscalDto.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,6 +11,9 @@ namespace api.Dtos.Fiscal
         public string Nome { get; set; } = string.Empty;
         public string Cpf { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
-        public string Senha { get; set; } = string.Empty; 
+
+        [Required]
+        [MinLength(6)]
+        public string Senha { get; set; } = string.Empty;
     }
 }
